f68ffc12cfa8e36609585eb882472f7f8be41ec9
“jtorp” <j.kontakt234@gmail.com>
2022-06-14 10:06:44 +0200

    not-found update

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,4 +1,4 @@
 const notFound = (req,res) =>{
-   res.status(404).send('<h1 style="color: red; margin-top: 5%;">Sorry -  page not Found</h1>')
+   res.status(404).send('<iframe style="display:flex; filter: saturate(2); margin: auto;"src = "https://giphy.com/embed/C21GGDOpKT6Z4VuXyn" width = "150" height = "210" frameBorder = "0"></iframe > ')
 }
 module.exports = notFound
